ad39719cd043eb5e9cd58e04ef8a9c9da9f5d0b7
Nelson Malgas <zmalgas69@gmail.com>
2025-09-22 14:49:18 +0200

    Update firebaseConfig to use environment variables

--- db/index.ts
+++ db/index.ts
@@ -9,13 +9,14 @@ import {
 import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCa-cJyunBNOXkDPissft5kmunPmIC6QIk",
-  authDomain: "connect-up-6cc47.firebaseapp.com",
-  projectId: "connect-up-6cc47",
-  storageBucket: "connect-up-6cc47.firebasestorage.app",
-  messagingSenderId: "205242905726",
-  appId: "1:205242905726:web:99420520c97e00c9c9142f",
-  measurementId: "G-Y17JB814N3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
 // Initialize Firebase
